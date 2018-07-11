92d695cefdfca88ac641df78bdf8e87d20e038e5
vedantm8 <vedantm8@gmail.com>
2018-07-10 20:18:08 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//Twitter
+var twitter = require('twitter');
+//Spotify
+var spotify = require('node-spotify-api');
+//Request
+var request = require('request');
+require('dotenv').config();
+//Inquirer - You use this to ask questions
+var inquirer = require('inquirer');
+
